ae7c9644945bb17176ddf77040e5f4b167e41e95
nguye <nguye@10.0.0.233>
2023-10-07 13:52:59 -0700

    Finished Main.java comments

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,20 +9,30 @@ import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
+	// Start method is entry point for JavaFx applications
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			// Loads the main user interface layout from "Main.fxml" file
 			HBox mainBox = (HBox)FXMLLoader.load(getClass().getClassLoader().getResource("view/Main.fxml"));
+			
+			// Create a new scene and associates it with the main user interface
 			Scene scene = new Scene(mainBox);
+			
+			// Adds CSS stylesheet to the scene
 			scene.getStylesheets().add(getClass().getClassLoader().getResource("css/application.css").toExternalForm());
+			
+			// Sets the primary stage scene and makes it visible
 			primaryStage.setScene(scene);
 			primaryStage.show();
 			
+			// Initialize and configure a shared instance of CommonObjs
 			CommonObjs commonObjs = CommonObjs.getInstance();
 			commonObjs.setMainBox(mainBox);
 			
 			
 		} catch(Exception e) {
+			// Handles exceptions that may occur when starting application
 			e.printStackTrace();
 		}
 	}
