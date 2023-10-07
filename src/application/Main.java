22e1696bf436373cad3047e43416170fc607b7c5
johnnyn <nguye@10.0.0.233>
2023-10-07 13:45:50 -0700

    added import comment on Main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,6 @@
 package application;
-	
+
+// Import necessary JavaFX classes for building the application
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
