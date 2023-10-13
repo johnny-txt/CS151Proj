21df1a662a704ff099edc0971eeb9c1b9059cad2
Anthony Nguyen <antnguyen233@gmail.com>
2023-10-13 03:58:55 -0700

    made test to test change visibility of node

---
+++ src/application/controller/ProjectListController.java
@@ -0,0 +1,45 @@
+package application.controller;
+
+import java.io.IOException;
+import java.net.URL;
+
+import application.CommonObjs;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+
+public class ProjectListController {
+	
+	// Creates instance of CommonObjs to access common objects and data across the application
+	private CommonObjs commonObjs = CommonObjs.getInstance();
+	
+	@FXML private Button coolList;
+	
+	public void CreateProjectOperation() {
+		// Gets URL of the "ProjectCreation.fxml" file and loads the JavaFx scene graph
+		URL url = getClass().getClassLoader().getResource("view/ProjectCreation.fxml");
+		
+		try {
+			// Loads and AnchorPane for the ProjectCreation view
+			AnchorPane pane1 = (AnchorPane) FXMLLoader.load(url);
+			
+			HBox mainBox = commonObjs.getMainBox();
+			
+			// Checks if there is already a child in mainBox, and if so, removes  it
+			if(mainBox.getChildren().size() > 1) {
+				mainBox.getChildren().remove(1);
+			}
+			
+			// Adds pane1 to the mainBox
+			mainBox.getChildren().add(pane1);
+			
+			coolList.setVisible(false);
+		} catch (IOException e) {
+			// Handles any exception that may occur during the view loading process
+			e.printStackTrace();
+		}
+	}
+}
