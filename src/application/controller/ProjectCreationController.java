a2eb0dcac3389a35b50c1d88c4d88e53528b6bce
johnnyn <nguye@10.0.0.233>
2023-10-14 02:14:41 -0700

    add if statement to project creation

--- src/application/controller/ProjectCreationController.java
+++ src/application/controller/ProjectCreationController.java
@@ -60,51 +60,47 @@ public class ProjectCreationController {
 	}
 	
 	
-	@FXML 
-    public void CreateNewProjectOp() {
-		String projName = name.getText();
-        LocalDate theDate = date.getValue();
-        String desc = description.getText();
-		// Gets URL of the "HomePageWelcome.fxml" file and loads the JavaFx scene graph
-		URL url = getClass().getClassLoader().getResource("view/HomePageWelcome.fxml");
-		
-		try {
-			// Loads and AnchorPane for the HomepageWelcome view
-			AnchorPane pane1 = (AnchorPane) FXMLLoader.load(url);
-			
-			// Retrieve the mainBox from the commonObjs instance
-			HBox mainBox = commonObjs.getMainBox();
-			
-			// Checks if there is already a child in mainBox, and if so, removes  it
-			if(mainBox.getChildren().size() > 1) {
-				mainBox.getChildren().remove(1);
-			}
-			
-			
-			// Adds pane1 to the mainBox
-			mainBox.getChildren().add(pane1);
-			
-			AnchorPane lol = commonObjs.getProjectList();
-			
-			Node emptyListText = lol.getChildren().get(0);
-			Node projectList = lol.getChildren().get(3);
-			
-			emptyListText.setVisible(false);
-			projectList.setVisible(true);
+	@FXML
+	public void CreateNewProjectOp() {
+	    String projName = name.getText();
+	    LocalDate theDate = date.getValue();
+	    String desc = description.getText();
 
+	    // Check if any of the fields is empty
+	    if (projName.isEmpty() || theDate == null || desc.isEmpty()) {
+	        return;
+	    }
 
-			
-		} catch (IOException e) {
-			// Handles any exception that may occur during the view loading process
-			e.printStackTrace();
-		}
-	
+	    // If all fields are non-empty, create the project
+	    ProjectDAO dao = new ProjectDAO();
+	    ProjectBean proj = new ProjectBean(projName, theDate, desc);
+	    dao.createProjectTable();
+	    dao.insertProject(proj);
 
-        ProjectDAO dao = new ProjectDAO();
-        ProjectBean proj = new ProjectBean(projName, theDate, desc);
-        dao.createProjectTable();
-        dao.insertProject(proj);
-        
-    }
+	    // Continue with page change
+	    URL url = getClass().getClassLoader().getResource("view/HomePageWelcome.fxml");
+
+	    try {
+	        AnchorPane pane1 = (AnchorPane) FXMLLoader.load(url);
+
+	        HBox mainBox = commonObjs.getMainBox();
+
+	        if (mainBox.getChildren().size() > 1) {
+	            mainBox.getChildren().remove(1);
+	        }
+
+	        mainBox.getChildren().add(pane1);
+
+	        AnchorPane lol = commonObjs.getProjectList();
+
+	        Node emptyListText = lol.getChildren().get(0);
+	        Node projectList = lol.getChildren().get(3);
+
+	        emptyListText.setVisible(false);
+	        projectList.setVisible(true);
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
+	}
 	
 }
