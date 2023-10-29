11dcaeb53b8eaa465c8017a1dd1f180383be0413
Anthony Nguyen <antnguyen233@gmail.com>
2023-10-28 19:40:50 -0700

    changed text list to buttons

--- src/application/controller/ProjectButtonController.java
+++ src/application/controller/ProjectButtonController.java
@@ -24,6 +24,8 @@ public class ProjectButtonController {
 		// Gets URL of the "ProjectCreation.fxml" file and loads the JavaFx scene graph
 		URL url = getClass().getClassLoader().getResource("view/ProjectBox.fxml");
 		URL ticketListUrl = getClass().getClassLoader().getResource("view/ProjectTicketList.fxml");
+		URL ticketUrl = getClass().getClassLoader().getResource("view/ticketButton.fxml");
+		
 		try {
 			commonObjs.setCurrentProject(commonObjs.getList().getChildren().indexOf(projectButton) + 1);
 			// Loads and AnchorPane for the ProjectCreation view
@@ -34,14 +36,17 @@ public class ProjectButtonController {
 			
 			pane1.getChildren().add(commonObjs.getTicketList());
 			
+			
 			for (int ticketID : Main.ticketDao.getTicketIDs()) {
 				int ticketProjectID = Main.ticketDao.getTicketProjectByID(ticketID);
 				String ticketName = Main.ticketDao.getTicketNameByID(ticketID);
 				if (ticketProjectID == commonObjs.getCurrentProject()) {
-					Text ticketText = new Text(ticketName);
-					ticketList.getChildren().add(ticketText);
+					Button ticketButton = (Button) FXMLLoader.load(ticketUrl);
+					ticketButton.setText(ticketName);
+					ticketList.getChildren().add(ticketButton);
 				}
 			}
+			
 
 	        HBox mainBox = commonObjs.getMainBox();
 
