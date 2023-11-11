5af464b7cacccdc4daaa852393f77184330f8e35
johnnyn <nguye@10.0.0.233>
2023-11-11 02:25:38 -0800

    tried fixing search project bar

--- src/application/controller/TicketListController.java
+++ src/application/controller/TicketListController.java
@@ -4,9 +4,12 @@ import java.io.IOException;
 import java.net.URL;
 
 import application.CommonObjs;
+import application.data_access_objects.TicketDAO;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -14,9 +17,13 @@ import javafx.scene.layout.VBox;
 public class TicketListController {
 	private CommonObjs commonObjs = CommonObjs.getInstance();
 	
+	public TicketDAO ticketDAO;
+	
 	@FXML public VBox list;
 	
 	@FXML public Label EmptyListText;
+	
+	@FXML public TextField SearchBar;
 	
 	public void CreateTicketOperation() {
 		// Gets URL of the "ProjectCreation.fxml" file and loads the JavaFx scene graph
@@ -63,4 +70,53 @@ public class TicketListController {
             e.printStackTrace();
         }
     }
+	
+	public void Search() {
+		
+		// URL for the "ProjectBox.fxml" file
+		String query = SearchBar.getText();
+		URL ticketBoxUrl = getClass().getClassLoader().getResource("view/ProjectBox.fxml");
+	    URL ticketUrl = getClass().getClassLoader().getResource("view/ticketButton.fxml");
+		try {
+			
+			// Load AnchorPane for the ProjectBox view
+			AnchorPane ticketBox = (AnchorPane) FXMLLoader.load(ticketBoxUrl);
+			
+			// Retrieve the mainBox from commonObjs
+			HBox mainBox = commonObjs.getMainBox();
+			
+			// If there is a view page in mainBox, remove it
+			if (mainBox.getChildren().size() > 1) {
+				mainBox.getChildren().remove(1);
+			}
+			
+			// Adds pane1 to the mainBox
+            mainBox.getChildren().add(ticketBox);
+            
+			VBox ticketList = commonObjs.getTicketList();
+			ticketList.getChildren().clear();
+			
+			for (int ticketID : ticketDAO.getTicketIDs()) {
+				int ticketProjectID = ticketDAO.getTicketProjectByID(ticketID);
+				String ticketName = ticketDAO.getTicketNameByID(ticketID);
+				String ticketDesc = ticketDAO.getTicketDescByID(ticketID);
+				
+				// Check if the ticket belongs to the current project
+				if (ticketProjectID == commonObjs.getCurrentProject() && ticketName.contains(query)) {
+					System.out.println(commonObjs.getCurrentProject());
+					
+					// Create a button for the ticket and add it to box1
+					Button ticketButton = (Button) FXMLLoader.load(ticketUrl);
+					ticketButton.setText("Ticket Name: " + ticketName + "     Desc: " + ticketDesc);
+					ticketList.getChildren().add(ticketButton);
+				}
+			}
+			
+			ticketBox.getChildren().add(ticketList);
+		
+		// Handles exceptions
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
