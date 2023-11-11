b4cd937fb12056d965d562950426fe6bafc63a84
johnnyn <nguye@10.0.0.233>
2023-11-10 19:23:55 -0800

    added comments to CommonObjs

--- src/application/CommonObjs.java
+++ src/application/CommonObjs.java
@@ -5,91 +5,106 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
+/**
+ * The CommonObjs class represents a singleton container for common user interface components and state
+ * used in a bug tracker software application.
+ */
 public class CommonObjs {
-	// Creates a private static instance of CommonObjs for the singleton pattern
-	private static CommonObjs commonObjs =  new CommonObjs();
-	
-	// Private field to hold the reference to the HBox representing the main user interface
-	private HBox mainBox;
-	private AnchorPane projectList;
-	private VBox list;
-	private VBox ticketList;
-	private VBox commentList;
-	private ChoiceBox<String> projectDropdown;
-	private int currentProjectID;
-	private int currentTicketID;
-	
-	// Instantiates CommonObjs only within this class
-	private CommonObjs() {}
-	
-	// Static method to retrieve the singleton instance of CommonObjs
-	public static CommonObjs getInstance(){
-		return commonObjs;
-	}
-	
-	// Getter method to access the mainBox HBox from outside the class
-	public HBox getMainBox() {
-		return mainBox;
-	}
-	
-	// Setter method to set the mainBox HBox from outside the class
-	public void setMainBox(HBox mainBox) {
-		this.mainBox = mainBox;
-	}
-	
-	public AnchorPane getProjectList() {
-		return projectList;
-	}
-	
-	public void setProjectList(AnchorPane projectList) {
-		this.projectList = projectList;
-	}
-	
-	public VBox getList() {
-		return list;
-	}
-	
-	public void setList(VBox list) {
-		this.list = list;
-	}
-	
-	public VBox getTicketList() {
-		return ticketList;
-	}
-	
-	public void setTicketList(VBox list) {
-		this.ticketList = list;
-	}
-	
-	public ChoiceBox<String> getProjectDropdown() {
-		return projectDropdown;
-	}
-	
-	public void setProjectDropdown(ChoiceBox<String> dropdown) {
-		this.projectDropdown = dropdown;
-	}
-	
-	public VBox getCommentList() {
-		return commentList;
-	}
-	
-	public void setCommentList(VBox list) {
-		this.commentList = list;
-	}
-	
-	public int getCurrentProject() {
-		return currentProjectID;
-	}
-	
-	public void setCurrentProject(int id) {
-		this.currentProjectID = id;
-	}
-	
-	public int getCurrentTicket() {
-		return currentTicketID;
-	}
-	
-	public void setCurrentTicket(int id) {
-		this.currentTicketID = id;
-	}
+    // Creates a private static instance of CommonObjs for the singleton pattern
+    private static CommonObjs commonObjs = new CommonObjs();
+
+    // Private field to hold the reference to the main HBox representing the primary user interface
+    private HBox mainBox;
+    
+    // Fields to store various UI components
+    private AnchorPane projectList;  // Represents the list of projects
+    private VBox list;  // Represents a generic list used in the application
+    private VBox ticketList;  // Represents the list of tickets within a project
+    private VBox commentList;  // Represents the list of comments for a specific ticket
+    private ChoiceBox<String> projectDropdown;  // Represents a dropdown for selecting projects
+    
+    // Fields to store the currently selected project and ticket IDs
+    private int currentProjectID;
+    private int currentTicketID;
+
+    // Instantiates CommonObjs only within this class
+    private CommonObjs() {}
+
+    // Static method to retrieve the singleton instance of CommonObjs
+    public static CommonObjs getInstance() {
+        return commonObjs;
+    }
+
+    // Getter method to access the mainBox HBox from outside the class
+    public HBox getMainBox() {
+        return mainBox;
+    }
+
+    // Setter method to set the mainBox HBox from outside the class
+    public void setMainBox(HBox mainBox) {
+        this.mainBox = mainBox;
+    }
+
+    // Getter and setter methods for the projectList field
+    public AnchorPane getProjectList() {
+        return projectList;
+    }
+
+    public void setProjectList(AnchorPane projectList) {
+        this.projectList = projectList;
+    }
+
+    // Getter and setter methods for the list field
+    public VBox getList() {
+        return list;
+    }
+
+    public void setList(VBox list) {
+        this.list = list;
+    }
+
+    // Getter and setter methods for the ticketList field
+    public VBox getTicketList() {
+        return ticketList;
+    }
+
+    public void setTicketList(VBox list) {
+        this.ticketList = list;
+    }
+
+    // Getter and setter methods for the projectDropdown field
+    public ChoiceBox<String> getProjectDropdown() {
+        return projectDropdown;
+    }
+
+    public void setProjectDropdown(ChoiceBox<String> dropdown) {
+        this.projectDropdown = dropdown;
+    }
+
+    // Getter and setter methods for the commentList field
+    public VBox getCommentList() {
+        return commentList;
+    }
+
+    public void setCommentList(VBox list) {
+        this.commentList = list;
+    }
+
+    // Getter and setter methods for the currentProjectID field
+    public int getCurrentProject() {
+        return currentProjectID;
+    }
+
+    public void setCurrentProject(int id) {
+        this.currentProjectID = id;
+    }
+
+    // Getter and setter methods for the currentTicketID field
+    public int getCurrentTicket() {
+        return currentTicketID;
+    }
+
+    public void setCurrentTicket(int id) {
+        this.currentTicketID = id;
+    }
 }
